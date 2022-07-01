2f87c0e71beedda0fb95feb26225d32d5cc6f8df
schlattk <konrad@pensionbee.com>
2022-07-01 13:25:48 +0100

    no version

--- orchestrator_poc/setup.py
+++ orchestrator_poc/setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     name="orchestrator_poc",
     packages=setuptools.find_packages(exclude=["orchestrator_poc_tests"]),
     install_requires=[
-        "dagster>=0.14.19",
-        "dagit>=0.14.19",
+        "dagster",
+        "dagit",
         "pytest",
         "dagster-dbt",
         "dagster-airbyte",
