82a5e528ad408cf55e188547f008e17507491757
schlattk <konrad@pensionbee.com>
2022-07-06 13:19:34 +0100

    name

--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -8,7 +8,7 @@ sync_google = airbyte_sync_op.configured(
 
 sync_salesforce = airbyte_sync_op.configured(
     {"connection_id": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"},
-    name="sync_salesforce"
+    name="salesforce_stronger_nudge"
 )
 
 def ssh():
