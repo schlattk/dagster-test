862153547dec28f0cb2240f01368bef4c7ef3814
schlattk <konrad@pensionbee.com>
2022-07-06 17:48:01 +0100

    exec

--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -16,7 +16,7 @@ def ssh():
     hostname = "ec2-52-215-25-222.eu-west-1.compute.amazonaws.com"
     username = "ec2-user"
     key_path = "/home/ec2-user/.ssh/airbyte_key"
-    sync_query = 'curl -X POST localhost:8000/api/v1/connections/sync -H "Content-Type: application/json" -d {"connectionId": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"}'
+    sync_query = 'curl -X POST localhost:8000/api/v1/connections/sync / -H "Content-Type: application/json" / -d {"connectionId": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"}'
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname, username=username, key_filename=key_path)
     ssh.exec_command(sync_query)
