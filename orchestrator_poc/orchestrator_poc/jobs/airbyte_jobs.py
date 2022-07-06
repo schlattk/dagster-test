aeeafcd1d4fc744f98dbc177240c1268ad61a581
schlattk <konrad@pensionbee.com>
2022-07-06 10:48:25 +0100

    ssh client

--- orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
@@ -1,6 +1,6 @@
 from dagster import job
 from dagster_airbyte import airbyte_resource, airbyte_sync_op
-from orchestrator_poc.ops.airbyte_ops import sync_google, sync_salesforce, ssh_connection
+from orchestrator_poc.ops.airbyte_ops import sync_google, sync_salesforce, ssh
 
 new_airbyte_resource = airbyte_resource.configured(
     {
@@ -12,5 +12,5 @@ new_airbyte_resource = airbyte_resource.configured(
 
 @job(resource_defs={"airbyte":new_airbyte_resource})
 def run_airbyte():
-    ssh_connection()
+    ssh()
     sync_salesforce()
--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -12,9 +12,8 @@ def ssh():
     username = "ec2-user"
     key_path = "/home/ec2-user/.ssh/airbyte_key"
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    return ssh.connect(hostname, username=username, key_filename=key_path)
-
-ssh_connection = ssh()
+    ssh.connect(hostname, username=username, key_filename=key_path)
+    return ssh
 
 sync_salesforce = airbyte_sync_op.configured(
     {"connection_id": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"},
