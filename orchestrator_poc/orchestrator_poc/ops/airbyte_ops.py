3ed1e63cc10ded8fcc4fb1787afedb4688fee479
schlattk <konrad@pensionbee.com>
2022-07-07 17:22:29 +0100

    kl

--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -28,5 +28,4 @@ def ssh():
     sync_query = "curl -X POST localhost:8000/api/v1/connections/sync  \ -H 'Content-Type: application/json' \ -d '{\"connectionId\": \"c1a5fdf3-903f-4d86-8601-5b6462afe40e\"}'"
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname, username=username, key_filename=key_path)
-    return client
     
