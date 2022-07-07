91b7aecce278b2132a6ddcbe5d1bc925f53c5986
schlattk <konrad@pensionbee.com>
2022-07-07 17:09:38 +0100

    kl

--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -14,7 +14,7 @@ sync_salesforce = airbyte_sync_op.configured(
 
 @op
 def ssh():
-    ssh = paramiko.SSHClient()
+    client = paramiko.SSHClient()
     hostname = "ec2-52-215-25-222.eu-west-1.compute.amazonaws.com"
     username = "ec2-user"
     key_path = "/home/ec2-user/.ssh/airbyte_key"
@@ -26,16 +26,6 @@ def ssh():
                             > error.txt
                     """
     sync_query = "curl -X POST localhost:8000/api/v1/connections/sync  \ -H 'Content-Type: application/json' \ -d '{\"connectionId\": \"c1a5fdf3-903f-4d86-8601-5b6462afe40e\"}'"
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        ssh.connect(hostname, username=username, key_filename=key_path)
-    except:
-        ssh.exec_command('touch before.txt')
-    # try:
-    #     ssh.exec_command('touch before.txt')
-    #     sync_salesforce()
-    #     ssh.exec_command('touch after.txt')
-    #     # ssh.exec_command(curl_command)
-    # except:
-    #     ssh.exec_command('touch error.txt')
-    # ssh.close()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, username=username, key_filename=key_path)
+    return client
