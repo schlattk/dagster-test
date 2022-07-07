2f45f8b9982f77672e4c46ef4d7c950d8776706f
schlattk <konrad@pensionbee.com>
2022-07-07 15:49:50 +0100

    post

--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -20,12 +20,12 @@ def ssh():
     key_path = "/home/ec2-user/.ssh/airbyte_key"
     url = "localhost:8000/api/v1/connections/sync"
     connection = '{"connectionId": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"}'
-    curl_command = f"""curl -X {url}
-                            -H "Content-Type: application/json"
+    curl_command = f"""curl -X POST {url}
+                            -H 'Content-Type: application/json'
                             -d {connection}
                             > error.txt
                     """
-    sync_query = "curl -X POST localhost:8000/api/v1/connections/sync -H \"Content-Type: application/json\" -d \"{'connectionId': 'c1a5fdf3-903f-4d86-8601-5b6462afe40e'}\""
+    sync_query = curl -X POST localhost:8000/api/v1/connections/sync -H 'Content-Type: application/json' -d '{"connectionId": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"}'"
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         ssh.connect(hostname, username=username, key_filename=key_path)
