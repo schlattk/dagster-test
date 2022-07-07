1187d6623b04ac336b3873d8f2c06a042bf80bb1
schlattk <konrad@pensionbee.com>
2022-07-07 10:10:11 +0100

    doc string

--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -18,14 +18,20 @@ def ssh():
     hostname = "ec2-52-215-25-222.eu-west-1.compute.amazonaws.com"
     username = "ec2-user"
     key_path = "/home/ec2-user/.ssh/airbyte_key"
-    sync_query = 'curl -X POST localhost:8000/api/v1/connections/sync / -H "Content-Type: application/json" / -d {"connectionId": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"}'
+    url = "localhost:8000/api/v1/connections/sync"
+    connection = "{'connectionId': 'c1a5fdf3-903f-4d86-8601-5b6462afe40e'}"
+    curl_command = f"""curl -X {URL}
+                            -H "Content-Type: application/json"
+                            -d {connection}
+                    """
+    sync_query = "curl -X POST localhost:8000/api/v1/connections/sync -H \"Content-Type: application/json\" -d \"{'connectionId': 'c1a5fdf3-903f-4d86-8601-5b6462afe40e'}\""
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         ssh.connect(hostname, username=username, key_filename=key_path)
     except:
-        touch('connection failed')
+        print('connection failed')
     try:
-        ssh.exec_command('touch check.txt')
+        ssh.exec_command(curl_command)
     except:
         print('Error')
     ssh.close()
