ddfe30c8572ef4f40989cdd3c2893f35ea9ff881
schlattk <konrad@pensionbee.com>
2022-07-08 15:35:14 +0100

    airbyte_ssh

--- orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
@@ -1,6 +1,6 @@
 from dagster import job
 from dagster_airbyte import airbyte_resource, airbyte_sync_op
-from orchestrator_poc.ops.airbyte_ops import sync_google, sync_salesforce, ssh
+from orchestrator_poc.ops.airbyte_ops import sync_google, sync_salesforce, airbyte_ssh
 
 new_airbyte_resource = airbyte_resource.configured(
     {
@@ -12,4 +12,4 @@ new_airbyte_resource = airbyte_resource.configured(
 
 @job(resource_defs={"airbyte":new_airbyte_resource})
 def run_airbyte():
-    sync_salesforce(start_after=ssh())
+    airbyte_ssh()
