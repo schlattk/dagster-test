e302255c0bf77c4189e870fbfccdd8013f3e4f74
schlattk <konrad@pensionbee.com>
2022-07-11 13:32:14 +0100

    corr

--- orchestrator_poc/orchestrator_poc/repository.py
+++ orchestrator_poc/orchestrator_poc/repository.py
@@ -2,8 +2,6 @@ from dagster import repository
 
 from orchestrator_poc.jobs.say_hello import run_in_order
 from orchestrator_poc.jobs.airbyte_jobs import run_dagster_airbyte
-from orchestrator_poc.jobs.dbt_jobs import run_dbt
-from orchestrator_poc.jobs.airbyte_dbt import run_airbyte_dbt
 from orchestrator_poc.schedules.my_hourly_schedule import basic_schedule
 from orchestrator_poc.sensors.my_sensor import my_sensor
 
@@ -15,7 +13,7 @@ def orchestrator_poc():
     For hints on building your Dagster repository, see our documentation overview on Repositories:
     https://docs.dagster.io/overview/repositories-workspaces/repositories
     """
-    jobs = [run_in_order, run_airbyte, run_dbt, run_airbyte_dbt]
+    jobs = [run_in_order, run_dagster_airbyte]
     schedules = [basic_schedule]
     sensors = [my_sensor]
 
