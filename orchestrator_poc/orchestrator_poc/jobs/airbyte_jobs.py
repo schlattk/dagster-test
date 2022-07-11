8af0da97343463d1276bc1879c877bad5a3626e0
schlattk <konrad@pensionbee.com>
2022-07-11 15:27:49 +0100

    construct

--- orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
@@ -4,7 +4,7 @@ from orchestrator_poc.ops.airbyte_ops import sync_google, sync_salesforce, airby
 
 new_airbyte_resource = airbyte_resource.configured(
     {
-        "host": "http://172.31.22.88:8000",
+        "host": "172.31.22.88:8000",
         "port": ""
     }
 )
