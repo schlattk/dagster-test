319a050ff57a7bb2c52c23f9cd3438ab07eda4a0
schlattk <konrad@pensionbee.com>
2022-07-12 14:27:40 +0100

    rpc sync

--- orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
@@ -1,11 +1,16 @@
 from dagster import job
-from dagster_dbt import dbt_rpc_resource, dbt_run_op
+from dagster_dbt import dbt_rpc_resource, dbt_run_op, dbt_rpc_sync_resource
 
 test_dbt_rpc_resource = dbt_rpc_resource.configured({
     "host": { "env": "DBT_HOST" },
     "port": { "env": "DBT_PORT" },
 })
 
-@job(resource_defs={ "dbt": test_dbt_rpc_resource })
+test_dbt_rpc_sync_resource = dbt_rpc_sync_resource.configured({
+    "host": { "env": "DBT_HOST" },
+    "port": { "env": "DBT_PORT" },
+})
+
+@job(resource_defs={ "dbt": test_dbt_rpc_sync_resource })
 def run_dbt():
     dbt_run_op()
