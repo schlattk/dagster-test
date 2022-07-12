fad0270c7b93da504f4e7099c9b36a37ddaaf594
schlattk <konrad@pensionbee.com>
2022-07-12 14:09:01 +0100

    format host

--- orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
@@ -1,11 +1,11 @@
 from dagster import job
 from dagster_dbt import dbt_rpc_resource, dbt_run_op
 
-dbt_rpc_resource = dbt_rpc_resource.configured(
-    "host": "$DBT_HOST" ,
-    "port": "$DBT_PORT",
-)
+test_dbt_rpc_resource = dbt_rpc_resource.configured({
+    "host": { "env": "DBT_HOST" },
+    "port": { "env": "DBT_PORT" },
+})
 
-@job(resource_defs={"dbt":dbt_rpc_resource})
+@job(resource_defs={ "dbt": test_dbt_rpc_resource })
 def run_dbt():
     dbt_run_op()
