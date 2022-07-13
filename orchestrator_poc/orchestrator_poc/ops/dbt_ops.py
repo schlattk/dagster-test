00b76f66a9b62901dd6955fd5ff9b41eea9c9d3d
schlattk <konrad@pensionbee.com>
2022-07-13 20:35:25 +0100

    rem

--- orchestrator_poc/orchestrator_poc/ops/dbt_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/dbt_ops.py
@@ -4,10 +4,10 @@ import paramiko
 @op
 def dbt_run_ssh():
     client = paramiko.SSHClient()
-    hostname =""
+    hostname ="ec2-34-244-209-39.eu-west-1.compute.amazonaws.com"
     username ="ec2-user"
     key_path = "/home/ec2-user/.ssh/airbyte_key"
-    run_command= "bash /home/ec2-user/run_job.sh"
+    run_command= "nohup bash /home/ec2-user/run_job.sh"
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname, username=username, key_filename=key_path)
     client.exec_command('touch before.txt')
