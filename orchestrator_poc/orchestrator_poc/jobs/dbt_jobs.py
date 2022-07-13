6b3dc06315ded81b5640cb8d50254da168fab322
schlattk <konrad@pensionbee.com>
2022-07-13 13:35:30 +0100

    ssh_dbt

--- orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
@@ -1,6 +1,6 @@
 from dagster import job
 from dagster_dbt import dbt_rpc_resource, dbt_run_op, dbt_rpc_sync_resource, dbt_rpc_run
-from orchestrator_poc.ops.dbt_ops import run_dbt_op
+from orchestrator_poc.ops.dbt_ops import dbt_run_ssh
 
 test_dbt_rpc_resource = dbt_rpc_resource.configured({
     "host": { "env": "DBT_HOST" },
@@ -15,3 +15,7 @@ test_dbt_rpc_sync_resource = dbt_rpc_sync_resource.configured({
 @job(resource_defs={ "dbt_rpc": test_dbt_rpc_resource })
 def run_dbt_job():
     dbt_rpc_run()
+
+@job
+def run_dbt_ssh_job():
+    dbt_run_ssh()
--- orchestrator_poc/orchestrator_poc/ops/dbt_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/dbt_ops.py
@@ -1,6 +1,16 @@
 from dagster import op
-from dagster_dbt import dbt_rpc_resource, dbt_run_op, dbt_rpc_sync_resource, dbt_rpc_run
+import paramiko
 
 @op
-def run_dbt_op():
-    dbt_run_op()
+def dbt_run_ssh():
+    client = paramiko.SSHClient()
+    hostname ="$DBT_SSH_HOST"
+    username ="$DBT_SSH_USER"
+    key_path = "/home/ec2-user/.ssh/airbyte_key"
+    run_command= "cd dbt_repo && dbt run"
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, username=username, key_filename=key_path)
+    client.exec_command('touch before.txt')
+    client.exec_command(run_command)
+    client.exec_command('touch after.txt')
+    client.close()
