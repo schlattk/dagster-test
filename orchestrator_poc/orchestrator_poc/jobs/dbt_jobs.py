943f1c2f60c4325e7e3ea49dc71922a8102c58eb
schlattk <konrad@pensionbee.com>
2022-07-13 11:19:45 +0100

    resource

--- orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
@@ -12,6 +12,6 @@ test_dbt_rpc_sync_resource = dbt_rpc_sync_resource.configured({
 })
 
 
-@job(resource_defs={ "dbt_rpc": test_dbt_rpc_sync_resource })
+@job(resource_defs={ "dbt": test_dbt_rpc_resource })
 def run_dbt_job():
     dbt_run_op()
