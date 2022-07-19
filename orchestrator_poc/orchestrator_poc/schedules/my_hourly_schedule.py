e49882410972e1f45e0184c4b39309fe78020b7b
schlattk <konrad@pensionbee.com>
2022-07-19 10:33:43 +0100

    schedule

--- orchestrator_poc/orchestrator_poc/schedules/my_hourly_schedule.py
+++ orchestrator_poc/orchestrator_poc/schedules/my_hourly_schedule.py
@@ -1,19 +1,20 @@
 from dagster import schedule, ScheduleDefinition
+from orchestrator_poc.jobs.airbyte_jobs import run_airbyte_and_then_dbt_cli
 
 
 
 
 
-# @schedule(cron_schedule="50 * * * *", job=say_hello_job)
-# def my_hourly_schedule():
-#     """
-#     A schedule definition. This example schedule runs once each hour.
-#
-#     For more hints on running jobs with schedules in Dagster, see our documentation overview on
-#     schedules:
-#     https://docs.dagster.io/overview/schedules-sensors/schedules
-#     """
-#     run_config = {}
-#     return run_config
+@schedule(cron_schedule="50 * * * *", job=run_airbyte_and_then_dbt_cli)
+def my_hourly_schedule():
+    """
+    A schedule definition. This example schedule runs once each hour.
 
-# basic_schedule = ScheduleDefinition(job=run_airbyte, cron_schedule="30 * * * *")
+    For more hints on running jobs with schedules in Dagster, see our documentation overview on
+    schedules:
+    https://docs.dagster.io/overview/schedules-sensors/schedules
+    """
+    run_config = {}
+    return run_config
+
+basic_schedule = ScheduleDefinition(job=run_airbyte_and_then_dbt_cli, cron_schedule="40 * * * *")
