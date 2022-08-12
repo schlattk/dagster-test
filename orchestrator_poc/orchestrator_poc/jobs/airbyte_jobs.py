c5a89e1087f4aca7b9d570496876fc0a03542685
schlattk <konrad@pensionbee.com>
2022-08-12 16:30:23 +0100

    add source

--- orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/airbyte_jobs.py
@@ -2,7 +2,7 @@ from dagster import job
 from dagster_airbyte import airbyte_resource, airbyte_sync_op
 from dagster_dbt import dbt_run_op
 from orchestrator_poc.jobs.dbt_jobs import test_dbt_rpc_sync_resource
-from orchestrator_poc.ops.airbyte_ops import sync_salesforce
+from orchestrator_poc.ops.airbyte_ops import sync_salesforce, sync_salesforce_fund
 
 new_airbyte_resource = airbyte_resource.configured(
     {
@@ -17,4 +17,4 @@ def run_dagster_airbyte():
 
 @job(resource_defs={"airbyte":new_airbyte_resource, "dbt": test_dbt_rpc_sync_resource})
 def run_airbyte_and_then_dbt_cli():
-    dbt_run_op(start_after=sync_salesforce())
+    dbt_run_op(start_after=[sync_salesforce(), sync_salesforce_fund()])
--- orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/airbyte_ops.py
@@ -1,8 +1,12 @@
 from dagster import op
 from dagster_airbyte import airbyte_sync_op
-import paramiko
 
 sync_salesforce = airbyte_sync_op.configured(
     {"connection_id": "c1a5fdf3-903f-4d86-8601-5b6462afe40e"},
     name="salesforce_stronger_nudge"
+)
+
+sync_salesforce_fund = airbyte_sync_op.configured(
+    {"connection_id": "dd66a499-7881-4e08-a63a-21dc63925ebb"},
+    name="salesforce_fund"
 )
