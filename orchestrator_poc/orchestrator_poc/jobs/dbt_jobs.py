2e7463d5e12771e917c776c0b806aa97584b5f7d
schlattk <konrad@pensionbee.com>
2022-09-26 17:04:45 +0100

    dbt deps

--- orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
+++ orchestrator_poc/orchestrator_poc/jobs/dbt_jobs.py
@@ -1,5 +1,6 @@
 from dagster import job
 from dagster_dbt import dbt_run_op, dbt_seed_op, dbt_test_op, dbt_rpc_sync_resource
+from orchestrator_poc.ops.dbt_ops import dbt_deps_op
 
 
 test_dbt_rpc_sync_resource = dbt_rpc_sync_resource.configured({
@@ -20,5 +21,9 @@ def seed_dbt_job():
     dbt_seed_op()
 
 @job(resource_defs={ "dbt": test_dbt_rpc_sync_resource })
+def deps_dbt_job():
+    dbt_deps_op()
+
+@job(resource_defs={ "dbt": test_dbt_rpc_sync_resource })
 def full_dbt_job():
-    dbt_test_op(start_after=[dbt_run_op(start_after=[dbt_seed_op()])])
+    dbt_test_op(start_after=[dbt_run_op(start_after=[dbt_seed_op(start_after=[dbt_deps_op()])])])
--- orchestrator_poc/orchestrator_poc/ops/dbt_ops.py
+++ orchestrator_poc/orchestrator_poc/ops/dbt_ops.py
@@ -6,3 +6,7 @@ dbt_run_project_op = dbt_run_op
 dbt_seed_project = dbt_seed_op
 
 dbt_test_project = dbt_test_op
+
+@op(required_resource_keys={"dbt"})
+def dbt_deps_op(context):
+    context.resources.dbt.cli("deps")
